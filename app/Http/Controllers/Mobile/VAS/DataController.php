059dc0ac5b6b90ad21ddbd42652fb0d82d0eca63
Jimmy <toluadejimi@gmail.com>
2025-04-12 17:56:07 +0100

    export terminal by  banks added

--- app/Http/Controllers/Mobile/VAS/DataController.php
+++ app/Http/Controllers/Mobile/VAS/DataController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Mobile\VAS;
 
 use App\Http\Controllers\Controller;
+use App\Models\Account;
 use App\Models\Setting;
+use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class DataController extends Controller
 {
@@ -68,5 +73,119 @@ class DataController extends Controller
 
 
     }
+
+    public function buy_data(Request $request)
+    {
+
+        $referenceCode = "EMRVAS-" . random_int(100, 999);
+        $sender_account_no = $request->senderAccountNo;
+        $amount = $request->Amount;
+
+        $can_transfer = User::where('id', Auth::id())->first()->can_transfer;
+        $account_tier = Account::where('user_id', Auth::id())->first()->account_tier;
+        $set = Setting::where('id', 1)->first();
+        $get_balance = $this->bankOneService->get_balance($sender_account_no);
+        $balance = $get_balance['availabe_balance'];
+        $final_tranferaable_amount = $amount;
+        if($balance < $final_tranferaable_amount){
+            return response()->json([
+                'status' => false,
+                'message' => "Insufficient Funds"
+            ], 422);
+        }
+
+
+        if ($set->bill == 0) {
+            return response()->json([
+                'status' => false,
+                'message' => "You can not buy data at the moment, Please contact support"
+            ], 422);
+        }
+
+
+
+
+        if ($account_tier == "Tier_1") {
+            $limit = $set->tier_1_limit;
+        } elseif ($account_tier == "Tier_2")
+            $limit = $set->tier_2_limit;
+        else {
+            $limit = $set->tier_3_limit;
+        }
+
+
+        if ($request->amount > $limit) {
+            return response()->json([
+                'status' => false,
+                'message' => "Upgrade your account to transfer more, Please contact support"
+            ], 422);
+        }
+
+
+        $data = [
+            'Amount' => $amount * 100,
+            'AccountNumber' => $sender_account_no,
+            'RetrievalReference' => $referenceCode,
+            'Narration' => "Debit for Data",
+        ];
+
+        $response = ['ResponseCode' => "00", 'Reference' => "000000000"]; //$this->bankOneService->initiate_customer_debit($data);
+
+
+
+        $set = Setting::where('id', 1)->first();
+        if($set->bills_provider == "vt_pass"){
+
+            try {
+
+                $apiKey = env('VTPASSAPIKEY');
+                $skKey = env('VTPASSSKKEY');
+                $requestId = date('YmdHis') . Str::random(4);
+
+
+                //Deduction
+
+                if($response['ResponseCode'] == "00"){
+                    $airtime = $this->VTpass->Pay($requestId, $request->service_id, $request->variation_code, $request->biller_code, $amount, $request->phone, $apiKey, $skKey);
+                    if ($airtime['status'] == true) {
+                        $trx = new Transaction();
+                        $trx->trx_ref = $referenceCode;
+                        $trx->user_id = Auth::id();
+                        $trx->transaction_type = "VAS";
+                        $trx->note = "Transaction successful";
+                        $trx->session_id = $airtime['requestId'];
+                        $trx->sender_account_no = $sender_account_no;
+                        $trx->fees = $set->transfer_charges;
+                        $trx->debit = $final_tranferaable_amount;
+                        $trx->amount = $request->Amount;
+                        $trx->status = 1;
+                        $trx->save();
+
+
+                        return response()->json([
+                            'status' => true,
+                            'message' => 'Data Purchase Successful',
+                        ]);
+                    }
+                }
+
+                return response()->json([
+                    'status' => false,
+                    'message' => "Data Purchase failed.",
+                ], 200);
+
+            } catch (\Exception $e) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'An unexpected error occurred: ' . $e->getMessage(),
+                ], 500);
+            }
+
+        }
+
+
+
+    }
+
 
 }
