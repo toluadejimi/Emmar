d28d274d93789e3b713707848833cd09430290c0
Jimmy <toluadejimi@gmail.com>
2025-04-12 15:10:23 +0100

    export terminal by  banks added

--- app/Http/Controllers/Mobile/BillsController.php
+++ app/Http/Controllers/Mobile/BillsController.php
@@ -27,6 +27,7 @@ class BillsController extends Controller
         $response = $this->VTpass->Validate($biller_code, $service_id);
 
 
+        dd($response);
 
         if($response['status'] == 1){
 
