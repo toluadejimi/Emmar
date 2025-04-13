e4301efa636f23a53edf16615798f1ed4369df67
Jimmy <toluadejimi@gmail.com>
2025-04-13 16:05:00 +0100

    export terminal by  banks added

--- app/Http/Controllers/Mobile/VAS/CableController.php
+++ app/Http/Controllers/Mobile/VAS/CableController.php
@@ -3,18 +3,26 @@
 namespace App\Http\Controllers\Mobile\VAS;
 
 use App\Http\Controllers\Controller;
+use App\Models\Account;
 use App\Models\Setting;
+use App\Models\Transaction;
+use App\Models\User;
+use App\Services\BankOneService;
 use App\Services\VTPassService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class CableController extends Controller
 {
 
-    protected $VTpass;
+    protected $bankOneService, $VTpass;
 
-    public function __construct(VTPassService $VTpass)
+    public function __construct(BankOneService $bankOneService, VTpassService $VTpass)
     {
+        $this->bankOneService = $bankOneService;
         $this->VTpass = $VTpass;
+
     }
 
     public function get_cable_biller()
@@ -101,5 +109,122 @@ class CableController extends Controller
 
 
     }
+
+
+    public function buy_cable(request $request)
+    {
+
+        $referenceCode = "EMRVAS-" . random_int(100, 999);
+        $sender_account_no = $request->senderAccountNo;
+        $amount = $request->Amount;
+
+        $can_transfer = User::where('id', Auth::id())->first()->can_transfer;
+        $account_tier = Account::where('user_id', Auth::id())->first()->account_tier;
+        $set = Setting::where('id', 1)->first();
+        $get_balance = $this->bankOneService->get_balance($sender_account_no);
+        $balance = $get_balance['availabe_balance'];
+
+        $final_tranferaable_amount = $amount + $set->eletric_charge;
+
+        if($balance < $final_tranferaable_amount){
+            return response()->json([
+                'status' => false,
+                'message' => "Insufficient Funds"
+            ], 422);
+        }
+
+
+        if ($set->bill == 0) {
+            return response()->json([
+                'status' => false,
+                'message' => "You can not buy data at the moment, Please contact support"
+            ], 422);
+        }
+
+
+
+
+        if ($account_tier == "Tier_1") {
+            $limit = $set->tier_1_limit;
+        } elseif ($account_tier == "Tier_2")
+            $limit = $set->tier_2_limit;
+        else {
+            $limit = $set->tier_3_limit;
+        }
+
+
+        if ($request->amount > $limit) {
+            return response()->json([
+                'status' => false,
+                'message' => "Upgrade your account to transfer more, Please contact support"
+            ], 422);
+        }
+
+
+        $data = [
+            'Amount' => $final_tranferaable_amount * 100,
+            'AccountNumber' => $sender_account_no,
+            'RetrievalReference' => $referenceCode,
+            'Narration' => "Debit for Data",
+        ];
+
+        $response = ['ResponseCode' => "00", 'Reference' => "000000000"]; //$this->bankOneService->initiate_customer_debit($data);
+
+
+
+        $set = Setting::where('id', 1)->first();
+        if($set->bills_provider == "vt_pass"){
+
+            try {
+
+                $apiKey = env('VTPASSAPIKEY');
+                $skKey = env('VTPASSSKKEY');
+                $requestId = date('YmdHis') . Str::random(4);
+
+
+                if($response['ResponseCode'] == "00"){
+                    $cable = $this->VTpass->Pay($requestId, $request->service_id, $request->variation_code, $request->biller_code, $amount, $request->phone, $apiKey, $skKey);
+                    if ($cable['status'] == true) {
+                        $trx = new Transaction();
+                        $trx->trx_ref = $referenceCode;
+                        $trx->user_id = Auth::id();
+                        $trx->transaction_type = "VAS";
+                        $trx->fees = $set->eletric_charge;
+                        $trx->vas_type = "cable";
+                        $trx->note = "Transaction Successful";
+                        $trx->session_id = $cable['requestId'];
+                        $trx->sender_account_no = $sender_account_no;
+                        $trx->debit = $final_tranferaable_amount;
+                        $trx->amount = $request->Amount;
+                        $trx->status = 1;
+                        $trx->save();
+
+
+                        return response()->json([
+                            'status' => true,
+                            'message' => "Transaction Successful",
+                        ]);
+                    }
+                }
+
+                return response()->json([
+                    'status' => false,
+                    'message' => "Cable Purchase failed.",
+                ], 200);
+
+            } catch (\Exception $e) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'An unexpected error occurred: ' . $e->getMessage(),
+                ], 500);
+            }
+
+        }
+
+
+
+
+    }
+
 
 }
